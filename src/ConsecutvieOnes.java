9bf9e462acb50ce8c4a0108f370d1a7e78df17da
mo-talha <mdtalha4488@gmail.com>
2025-04-14 08:26:14 +0530

    Problem: Max no of Consecutive Ones

--- src/ConsecutvieOnes.java
+++ src/ConsecutvieOnes.java
@@ -1,5 +1,25 @@
 public class ConsecutvieOnes {
     public static void main(String[] args) {
+        consecutiveOnes();
+    }
 
+    public static void consecutiveOnes(){
+        int [] nums = {1,0,1,1,0,1};
+        int onesCount = 0;
+
+        int maxNoOfOnes = 0;
+
+        for (int num: nums){
+            if (num == 1){
+                onesCount++;
+                if (onesCount > maxNoOfOnes){
+                    maxNoOfOnes = onesCount;
+                }
+            } else {
+                onesCount = 0;
+            }
+        }
+
+        System.out.println(maxNoOfOnes);
     }
 }
