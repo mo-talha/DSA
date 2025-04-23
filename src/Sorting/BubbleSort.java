9c6516529337572e088f9eb4ef31d385a4915b08
mo-talha <mdtalha4488@gmail.com>
2025-04-23 22:23:47 +0530

    Bubble Sort

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,38 @@
+package Sorting;
+
+import Utils.Utils;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+//        bubbleSort();
+        bubbleSort2();
+    }
+
+    public static void bubbleSort(){
+//        int [] nums = {2, 1, 3, 5, 4};
+        int [] nums = {4, 1, 5, 2, 3};
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] < nums[i-1]){
+                Utils.swap(nums, i, i-1);
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void bubbleSort2(){
+        int [] nums = {4, 1, 5, 2, 3};
+
+        for (int i = nums.length - 1; i >= 0; i--){
+            for (int j = 0; j < i; j++){
+                if (nums[j] > nums[j+1]){
+                    Utils.swap(nums, j, j+1);
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
