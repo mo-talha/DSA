e384dc9d0b7250d4b6e764dc369ee0a7de86e8ab
mo-talha <mdtalha4488@gmail.com>
2025-04-23 18:16:15 +0530

    Selection Sort

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        selectionSort();
+    }
+
+    public static void selectionSort(){
+        int [] nums = {3, 1, 5, 4};
+
+        int min = nums[0];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[j] < nums[i]){
+                    min = j;
+                }
+            }
+            swap(nums, i, min);
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void swap(int [] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
